946a70a719c11ebf2f06b203420729a35cdedf55
Maksim Ryzhikov <rv.maksim@gmail.com>
2019-07-28 23:45:00 +0300

    feat: print console log inside the editor

--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ doRender();
 
 function doRender() {
   const rootNode = document.getElementById('app');
-  const value = getSavedValue();
+  const value = localStorage.getItem('value') || '';
   render(h(App, {
     value: value,
-    onChange: saveValue,
+    onChange: (value) => localStorage.setItem('value', value),
     onRun: doEval
   }), rootNode, rootNode.lastChild);
 }
@@ -22,17 +22,16 @@ function doRender() {
 function doEval(code) {
   if (process) process.terminate();
   process = Executor.runAsync(code);
-  process.addEventListener('message', (it) => console.log(it.data));
+  process.addEventListener('message', (it) => {
+    if (it.loc) {
+      const line = it.loc.end.line;
+      const linode = document.querySelectorAll('.CodeMirror-line')[line - 1];
+      const textNode = lineNode.querySelector('.hint') || document.createElement('span');
+      if (!textNode.classList.contains('hint')) textNode.classList.add('hint');
+      textNode.innerText = it.data.join(', ');
+      lineNode.appendChild(textNode);
+    }
+  });
   process.addEventListener('error', (it) => console.error(it.message));
   doRender();
-}
-
-
-function getSavedValue() {
-  return localStorage.getItem('value') || '';
-}
-
-
-function saveValue(value) {
-  localStorage.setItem('value', value);
 }
